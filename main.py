48365d7d1e81143d659ca3e79bab0279b80ebd43
Willian Silva <williansilva@alu.ufc.br>
2026-07-05 17:51:29 -0300

    feat: improve user prompts and output messages in main experiment selection

--- main.py
+++ main.py
@@ -3,24 +3,44 @@ import experimentos.experimento_2 as exp2
 import experimentos.experimento_3 as exp3
 import experimentos.experimento_4 as exp4
 
+
 def main():
-    print("Hello from busca-ia!")
     print("Digite o número do experimento que deseja executar (0 para parar): ")
+    print("1 - Experimento 1")
+    print("2 - Experimento 2")
+    print("3 - Experimento 3")
+    print("4 - Experimento 4")
 
     while True:
         experimento = input()
 
-        if experimento == '0':
+        if experimento == "0":
             print("Encerrando o programa.")
             break
-        elif experimento == '1':
+        elif experimento == "1":
             exp1.run_experimento()
-        elif experimento == '2':
+            print(
+                "Experimento 1 concluído. Digite o número do próximo experimento (0 para parar): "
+            )
+        elif experimento == "2":
             exp2.run_experimento()
-        elif experimento == '3':
+            print(
+                "Experimento 2 concluído. Digite o número do próximo experimento (0 para parar): "
+            )
+        elif experimento == "3":
             exp3.run_experimento()
-        elif experimento == '4':
+            print(
+                "Experimento 3 concluído. Digite o número do próximo experimento (0 para parar): "
+            )
+        elif experimento == "4":
             exp4.run_experimento()
+            print(
+                "Experimento 4 concluído. Digite o número do próximo experimento (0 para parar): "
+            )
+        else:
+            print(
+                "Experimento inválido. Digite o número do experimento que deseja executar (0 para parar): "
+            )
 
 
 if __name__ == "__main__":
